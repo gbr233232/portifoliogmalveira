cf68b60ceba9fe9dd929ea3442e3f982745333bb
Gabriel <gabrielmalveira@edu.unifor.br>
2024-11-28 13:16:37 -0300

    ajustes

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ const app = express();
 //conexão mongoDB
 async function connectDB() {
     try{
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI, { 
+            useNewUrlParser: true, 
+            useUnifiedTopology: true 
+        });
         console.log('Conectado á base de dados')
         app.emit('pronto');
     }catch(err) {
